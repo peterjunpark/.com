01bedd10aa21e51ab98f46b0cf7cfd7d719c614b
Peter Jun Park <git@peterjunpark.com>
2025-07-26 01:45:06 -0400

    Add custom cursors

--- _config.ts
+++ _config.ts
@@ -1,4 +1,5 @@
 import lume from "lume/mod.ts";
+import transformImages from "lume/plugins/transform_images.ts";
 
 const site = lume({
     server: {
@@ -6,7 +7,10 @@ const site = lume({
     },
 });
 
+site.use(transformImages())
+
 site.add("public/styles.css", "styles.css");
+site.add("public/cursors", "cursors");
 site.copy("public/fonts", "fonts");
 
 export default site;
