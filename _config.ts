15c4ef79dbc110c5100a3f1d1e5b3cb7bcb2efe2
Peter Jun Park <git@peterjunpark.com>
2025-07-25 21:58:02 -0400

    fmt

--- _config.ts
+++ _config.ts
@@ -1,6 +1,10 @@
 import lume from "lume/mod.ts";
 
-const site = lume();
+const site = lume({
+    server: {
+        port: 6969,
+    },
+});
 
 site.add("public/styles.css", "styles.css");
 site.copy("public/fonts", "fonts");
