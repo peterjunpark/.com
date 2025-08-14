ae07471951f42cbc07397c9c7c5c21e0521e4c05
Peter Jun Park <git@peterjunpark.com>
2025-08-13 20:09:21 -0400

    wip: Update tasks and scripts for typst workflow

--- _config.ts
+++ _config.ts
@@ -2,15 +2,17 @@ import lume from "lume/mod.ts";
 import transformImages from "lume/plugins/transform_images.ts";
 
 const site = lume({
+    // Don't empty _site/ before the build.
+    emptyDest: false,
     server: {
         port: 6969,
     },
 });
 
-site.use(transformImages())
+site.use(transformImages());
 
 site.add("public/styles.css", "styles.css");
 site.add("public/cursors", "cursors");
-site.copy("public/fonts", "fonts");
+site.add("public/fonts", "fonts");
 
 export default site;
