c4ff396a9b4961af33fad7b51526b22cbd558cc2
Peter Jun Park <git@peterjunpark.com>
2025-08-16 14:56:11 -0400

    Add WebTUI and update assets
    fix css

--- _config.ts
+++ _config.ts
@@ -3,17 +3,15 @@ import transformImages from "lume/plugins/transform_images.ts";
 
 const site = lume({
     src: "./src",
+    location: new URL("https://peterjunpark.com"),
     // Don't empty _site/ before the build.
     emptyDest: false,
-    server: {
-        port: 6969,
-    },
+    server: { port: 6969 },
 });
 
-site.use(transformImages());
-
-site.add("public/styles.css", "styles.css");
-site.add("public/cursors", "cursors");
-site.add("public/fonts", "fonts");
+site.use(transformImages())
+    .add("styles")
+    .add("public/cursors", "cursors")
+    .add("public/fonts", "fonts");
 
 export default site;
